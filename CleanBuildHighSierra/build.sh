aa94dddecc0b13d267d5b1f2bf8f372809630712
Geoff Lee <g.lee@ed.ac.uk>
2018-09-05 10:09:01 +0100

    Bump version

--- CleanBuildHighSierra/build.sh
+++ CleanBuildHighSierra/build.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+VERSION=1.0.1
+RELEASE=1
+
 package=""
 echo "Enter path to quickadd package:"
 read package
@@ -21,6 +24,6 @@ cp ${package} ${tmpdir}/root/Library/MacSD
 
 chmod +x Scripts/*
 
-pkgbuild --root ${tmpdir}/root --scripts ./Scripts --id uk.ac.ed.eca.ECACleanBuildAndEnrol --version 1.0.0 _build/ECACleanBuildAndEnrol-1.0.0.pkg
+pkgbuild --root ${tmpdir}/root --scripts ./Scripts --id uk.ac.ed.eca.ECACleanBuildAndEnrol --version ${VERSION} _build/ECACleanBuildAndEnrol-${VERSION}-${RELEASE}.pkg
 
 rm -r ${tmpdir}
