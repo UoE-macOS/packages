1dc93e1e2ae58f664b0b4ac114fdc615905808f6
Geoff Lee <g.lee@ed.ac.uk>
2018-09-10 22:09:44 +0100

    Package build process now sets icons

--- macsd-weblocs/build.sh
+++ macsd-weblocs/build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eux -o pipefail
 
 VERSION=1.0.0
 RELEASE=1
-DIR='_build'
+BUILD='_build'
 
-[ -d "${DIR}" ] && rm -r ${DIR}
-mkdir ${DIR}
+[ -d "${BUILD}" ] && rm -r ${BUILD}
+mkdir ${BUILD}
 
-pkgbuild --version ${VERSION} --id uk.ac.ed.macsd.weblocs --root root "${DIR}/MacSDWeblocs-${VERSION}-${RELEASE}.pkg"
+# Fix up icons, which don't survive the transition to Git inside xattrs
+dest="${BUILD}/root/Library/MacSD/weblocs"
+mkdir -p "${dest}"
+
+cp weblocs/*.url "${dest}"
+
+ls "${dest}" | while read file
+do
+    file="${file%.*}"
+    cmd="import Cocoa; Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(
+        Cocoa.NSImage.alloc().initWithContentsOfFile_('icons/${file}.icns'), '${dest}/${file}.url', 0)"
+
+    python -c "${cmd}"
+    sleep 5
+done
+
+pkgbuild --version ${VERSION} --id uk.ac.ed.macsd.weblocs --root ${BUILD}/root "${BUILD}/MacSDWeblocs-${VERSION}-${RELEASE}.pkg"
+
+lsbom $(pkgutil --bom "${BUILD}/MacSDWeblocs-${VERSION}-${RELEASE}.pkg")
