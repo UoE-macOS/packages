467c611315f469f341fb9aba8ae4fe887692d13a
Geoff Lee <g.lee@ed.ac.uk>
2018-09-14 12:16:42 +0100

    Package pro tools extra instruments

---
+++ protools-extra-instruments/README.md
@@ -0,0 +1,9 @@
+Protools Extra Instruments
+==========================
+
+# Build
+`sh build.sh`
+
+You will be asked to pass the path to the Adobe AIR Instruments DMG
+
+This script will build a package that mounts the DMG and installs the content thereon 
---
+++ protools-extra-instruments/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file="ThisIsNotAFile"
+
+while [[ ! -f $file ]]
+do
+  echo "Path to the AIR Instruments .dmg file"
+  read file
+done
+
+cp "${file}" scripts/
+
+chmod +x scripts/postinstall
+pkgbuild --id com.github.uoe-macos.protools_air_instruments --nopayload --scripts scripts --version 2018.4 ProToolsExtraFirstAIRInstrumentsBundle-2018.4-1.pkg
+
