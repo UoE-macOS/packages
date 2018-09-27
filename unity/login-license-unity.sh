6f407cc47c9736d4d5484070278b86449e541ac7
Geoff Lee <g.lee@ed.ac.uk>
2018-09-27 17:18:47 +0100

    Worked out how to run unity as the logging-in user. Fix some prefs, too.

--- unity/login-license-unity.sh
+++ unity/login-license-unity.sh
@@ -3,23 +3,21 @@
 ###################################################################
 #
 # This script attempts to verify whether Unity is correctly licensed
-# and license it if not. It needs to be run as root but also with a 
-# user logged in (it uses the actual Unity application to perform the 
-# licensing operation). 
+# and license it if not. 
 # 
 # This script assumes it is being run as a login hook by Jamf Pro: as
-# such it expects arg1 to be the username of the logging-in user and 
+# such it expects arg3 to be the username of the logging-in user and 
 # the licensing data to be passed in args 4, 5 and 6.
 #
-# Date: @@DATE
-# Version: @@VERSION
-# Origin: @@ORIGIN
-# Released by JSS User: @@USER
+# Date: "Thu Sep 27 12:58:59 2018 +0100"
+# Version: 0.0.2
+# Origin: https://github.com/UoE-macOS/packages
+# Released by JSS User: glee1
 #
 ##################################################################
-set -euo pipefail
+set -euo pipefail 
 
-USER="${1}"
+USER="${3}"
 SERIAL="${4}"
 USERNAME="${5}"
 PASSWORD="${6}"
@@ -28,31 +26,49 @@ UNITY_PATH='/Applications/Unity/Unity.app/Contents/MacOS/Unity'
 LICENSE_PATH='/Library/Application Support/Unity/'
 LICENSE_FILE='Unity_lic.ulf'
 
+function set_prefs(){
+	# Try to get around unity's default behaviour 
+    # of creating new projects in the root of the user's
+    # home directory. Unfortunately tildae expansion doesn't 
+    # seem to work on these paths so we need to blat them
+    # in at login.
+    defaults write /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist kNewProjectsPath "/Users/$USER/Documents"
+    defaults write /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist kProjectBasePath "/Users/$USER/Documents/New Unity Project"
+    defaults write /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist kWorkspacePath "/Users/$USER/Documents"
+    
+    # These could probably be done by other means, but since we are here, 
+    # disable auto-upadte checking and remove the login screen on startup
+    defaults write /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist EditorUpdateShowAtStartup -int 0
+    defaults write /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist UnityConnectWorkOffline -int 1
+    chown $USER /Users/$USER/Library/Preferences/com.unity3d.UnityEditor5.x.plist
+}
+
 if [ -f "${LICENSE_PATH}/${LICENSE_FILE}" ]
 then
     echo "$0: Unity License file is already present. Exiting"
+    set_prefs
     exit 0
 fi
 
 echo "$0: No Unity license file found. Continuing"
-
-# Make sure the license path isn't writable by mere mortals, otherwise
-# users can inadvertantly relicense the app with their own login details.
-chown root:admin "${LICENSE_PATH}"
-chmod 755 "${LICENSE_PATH}"
-
 echo "$0: Running Unity licensing command."
 
-# CD to a temporary directory: running this command as root seems to spew
-# some stuff into the current working directory when Unity starts up.
+# Create a temporary directory to work in: running this command seems 
+# to spew ome stuff into the current working directory when Unity starts up.
 tmpdir="$(mktemp -d /tmp/unitylicense.XXXX)"
+
+# We are going to run unity as the logging-in user, so make sure
+# they can write to our working directory
+chmod 777 "${tmpdir}"
+
+# CD
 pushd "${tmpdir}"
 
 # Run the licensing command
 command="${UNITY_PATH} -quit -batchmode -serial ${SERIAL} -username ${USERNAME} -password ${PASSWORD}"
 
 command_result=0
-${command} || command_result=1
+sudo -u $USER ${command} || command_result=1
 
 # Clean up our temporary directory
 popd && rm -r "${tmpdir}"
@@ -72,6 +88,10 @@ then
     if [ -f "${LICENSE_PATH}/${LICENSE_FILE}" ]
     then
         echo "$0: That seemed to work."
+        # Make sure the license file is writable by everyone, otherwise
+		# unity complains on launch. It seems to want to update a timestamp
+		chmod 666 "${LICENSE_PATH}/${LICENSE_FILE}"
+        set_prefs
         exit 0
     else
         echo "$0: There is no license file, something went wrong"
