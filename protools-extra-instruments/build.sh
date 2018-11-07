06d0d67aff94d13a9952cd63950035261db166e9
Geoff Lee <g.lee@ed.ac.uk>
2018-11-07 09:48:12 +0000

    Make sure the name of the dmg inside the package matches

--- protools-extra-instruments/build.sh
+++ protools-extra-instruments/build.sh
@@ -8,7 +8,7 @@ do
   read file
 done
 
-cp "${file}" scripts/
+cp "${file}" scripts/ProToolsExtraFirstAIRInstrumentsBundle.dmg
 
 chmod +x scripts/postinstall
 pkgbuild --id com.github.uoe-macos.protools_air_instruments --nopayload --scripts scripts --version 2018.4 ProToolsExtraFirstAIRInstrumentsBundle-2018.4-1.pkg
